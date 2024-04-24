308d5df62c1f0e4fc6ebd358c43db3e6ded7737e
Sarita <sm7767119@gmail.com>
2024-04-24 13:37:24 +0530

    Node with SQL

---
+++ index.js
@@ -0,0 +1,35 @@
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "delta_app",
+  password: "sarita",
+});
+
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+// inserting new data
+let q = "INSERT INTO user(id, username, email, password) VALUES ?";
+let data = [];
+for (let i = 1; i <= 100; i++) {
+  data.push(getRandomUser());
+}
+
+try {
+  connection.query(q, [data], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE user(
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SHOW TABLES;
+-- source schema.sql -- run file
+
+
+
+
+
